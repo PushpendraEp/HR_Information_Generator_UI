2bffdabf34ca68b0e650820cefd7c9851c86f77b
keertisonii <Ep.kirtisoni@gmail.com>
2023-02-27 12:21:13 +0530

    dashboard page
    design dashboard page

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardMainComponent } from './dashboard-main/dashboard-main.component';
+import { DashboardHeaderComponent } from './dashboard-header/dashboard-header.component';
+import { DashboardFooterComponent } from './dashboard-footer/dashboard-footer.component';
+import { DashboardSelecterComponent } from './dashboard-selecter/dashboard-selecter.component';
 
 
 @NgModule({
   declarations: [
-    DashboardMainComponent
+    DashboardMainComponent,
+    DashboardHeaderComponent,
+    DashboardFooterComponent,
+    DashboardSelecterComponent
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    FormsModule
   ]
 })
 export class DashboardModule { }
