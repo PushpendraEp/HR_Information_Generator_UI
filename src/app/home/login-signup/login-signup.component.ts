4bf18b0eb8c027f672db3805f6e994082996168f
RaviBairagi10 <125532320+RaviBairagi10@users.noreply.github.com>
2023-02-28 15:26:25 +0530

    login Api integration
    worked on login api and do api integration from client side

--- src/app/home/login-signup/login-signup.component.ts
+++ src/app/home/login-signup/login-signup.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 import {NgForm} from '@angular/forms'
 
+import { HttpClient } from '@angular/common/http';
+import { UserService } from 'src/app/service/user.service';
+import { Router, Routes } from '@angular/router';
+
 @Component({
   selector: 'app-login-signup',
   templateUrl: './login-signup.component.html',
   styleUrls: ['./login-signup.component.css']
 })
 export class LoginSignupComponent {
-
+  constructor(private userService:UserService ,private route:Router) {}
+ 
  // <!-- @ kirti ( 17/02/23 ) Template Driven login form and reset value after submission--->
-  userlogin(item:NgForm){
-    console.log(item.value);
-    item.reset()
-   }
-
-}
+ //<!-- @ Ravi ( 28/02/23 ) login api integration -->
+ userLogin(data:any){
+  console.log(data)
+  this.userService.login(data)
+  this.route.navigate(['/dashboard'])
+ }
+ 
+} 
