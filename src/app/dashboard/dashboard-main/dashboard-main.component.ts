146c8b4ff20eb2768aa79fc58960283a60ec18e5
RaviBairagi10 <125532320+RaviBairagi10@users.noreply.github.com>
2023-03-02 16:46:12 +0530

    Upload file api integration
    Upload file from the client side and file store in mysqldatabase by the backend side.

--- src/app/dashboard/dashboard-main/dashboard-main.component.ts
+++ src/app/dashboard/dashboard-main/dashboard-main.component.ts
@@ -1,21 +1,29 @@
 // <!-- @ kirti soni ( 24/02/23 )  get data of file-->
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { UploadService } from 'src/app/service/upload.service';
 
 @Component({
   selector: 'app-dashboard-main',
   templateUrl: './dashboard-main.component.html',
-  styleUrls: ['./dashboard-main.component.css']
+  styleUrls: ['./dashboard-main.component.css'],
 })
 export class DashboardMainComponent {
-
-  selectfile(event: any){
-    console.warn(event.target.files)
-
+  constructor(private uploadService: UploadService) {}
+  file: any;
+  months:any;
+  years: any;
+  formData: any;
+  selectfile(event: any) {
+    this.file = event.target.files[0];
+    console.warn(this.file);
   }
-  userdata(item:NgForm){
-   
-    console.warn(item)
-
+  userdata(userForm: any) {
+    let formData = new FormData();
+    formData.append('file', this.file);
+    formData.append('selectedMonth', userForm.value.months);
+    formData.append('selectedYear', userForm.value.years);
+    console.log(formData);
+    this.uploadService.upload(formData);
   }
 }
